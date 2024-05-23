d7747ec3e73223913cb89fe98bde85ed047e8b3c
CatmanFan <oussama-studying@hotmail.com>
2024-05-23 18:52:18 +0100

    Icon

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,6 +29,7 @@ namespace DWMBG_AeroCalculator
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -97,6 +98,7 @@ namespace DWMBG_AeroCalculator
             this.Controls.Add(this.trackBar1);
             this.Font = new System.Drawing.Font("Tahoma", 8F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainForm";
