eed185e2e495ba437af92bed0eb9770b9cbf37e5
CatmanFan <oussama-studying@hotmail.com>
2024-05-23 03:58:13 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace DWMBG_AeroCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.WriteToConfig = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 12);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(466, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Primary balance:\r\nSecondary balance:\r\nBlur balance:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(131, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // WriteToConfig
+            // 
+            this.WriteToConfig.Location = new System.Drawing.Point(352, 116);
+            this.WriteToConfig.Name = "WriteToConfig";
+            this.WriteToConfig.Size = new System.Drawing.Size(122, 23);
+            this.WriteToConfig.TabIndex = 3;
+            this.WriteToConfig.Text = "Write to config";
+            this.WriteToConfig.UseVisualStyleBackColor = true;
+            this.WriteToConfig.Click += new System.EventHandler(this.WriteToConfig_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "INI files|*.ini|All files|*.*";
+            this.openFileDialog.SupportMultiDottedExtensions = true;
+            this.openFileDialog.Title = "Select a valid DWMBG configuration file...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(486, 151);
+            this.Controls.Add(this.WriteToConfig);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Font = new System.Drawing.Font("Tahoma", 8F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aero intensity calculator for DWMBlurGlass";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button WriteToConfig;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DWMBG_AeroCalculator
+{
+    public partial class MainForm : Form
+    {
+        double primary;
+        double secondary;
+        double blur;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            trackBar1.Value = Properties.Settings.Default.Opacity;
+            SetValues();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e) => SetValues();
+
+        private void SetValues()
+        {
+            float t = 26 + (217 * ((float)trackBar1.Value / (float)trackBar1.Maximum));
+            primary = t < 103 ? 5 : t < 188 ? 0.776471 * t - 74.976471 : t < 189 ? 71 : 0.535714 * t - 31.25;
+            secondary = t < 102 ? 0.526316 * t - 8.684211 : t < 189 ? -0.517241 * t + 97.758621 : 0;
+            blur = t < 102 ? -0.526316 * t + 103.684211 : t < 188 ? -0.255814 * t + 76.093023 : t < 189 ? 28 : -0.535714 * t + 131.25;
+            SetText();
+        }
+
+        private bool CheckValidity(string path)
+        {
+            if (!File.Exists(path)) return false;
+
+            bool valid = false;
+
+            foreach (string line in File.ReadAllLines(Path.Combine(path)))
+            {
+                if (line.Contains("aeroColorBalance") || line.Contains("aeroAfterglowBalance") || line.Contains("aeroBlurBalance"))
+                {
+                    valid = true;
+                    Properties.Settings.Default.ConfigFile = path;
+                    Properties.Settings.Default.Save();
+                }
+            }
+
+            return valid;
+        }
+
+        private void SetText() => label2.Text = $"{(primary > 0 ? primary.ToString("#.000") : "0.000")}\n{(secondary > 0 ? secondary.ToString("#.000") : "0.000")}\n{(blur > 0 ? blur.ToString("#.000") : "0.000")}";
+
+        private void WriteToConfig_Click(object sender, EventArgs e)
+        {
+            if (!CheckValidity(Properties.Settings.Default.ConfigFile))
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (!CheckValidity(openFileDialog.FileName))
+                    {
+                        MessageBox.Show("Not a valid configuration file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                }
+            }
+
+            // Write to files
+            // *************
+            var newConfig = File.ReadAllLines(Properties.Settings.Default.ConfigFile);
+            int modified = 0;
+
+            for (int i = 0; i < newConfig.Length; i++)
+            {
+                if (newConfig[i].StartsWith("aeroColorBalance"))            { newConfig[i] = $"aeroColorBalance={primary / 100}"; modified++; }
+                else if (newConfig[i].StartsWith("aeroAfterglowBalance"))   { newConfig[i] = $"aeroAfterglowBalance={secondary / 100}"; modified++; }
+                else if (newConfig[i].StartsWith("aeroBlurBalance"))        { newConfig[i] = $"aeroBlurBalance={blur / 100}"; modified++; }
+            }
+
+            if (modified < 3)
+            {
+                MessageBox.Show("Failed to modify all Aero blur values.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(Properties.Settings.Default.ConfigFile))
+                {
+                    foreach (var item in newConfig)
+                        sw.WriteLine(item);
+                }
+
+                MessageBox.Show("All values written to config.\nYou may need to uninstall and reinstall DWMBlurGlass from its GUI, or sign off Windows for changes to take effect.");
+
+                Properties.Settings.Default.Opacity = trackBar1.Value;
+                Properties.Settings.Default.Save();
+            }
+
+        }
+    }
+}
