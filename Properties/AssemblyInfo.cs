30770f2d2fbf97a5fada9ed161598c427fde9459
CatmanFan <oussama-studying@hotmail.com>
2024-05-24 15:38:48 +0100

    Removed UAC requirement

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DWMBG_AeroCalculator")]
+[assembly: AssemblyTitle("Aero intensity calculator for DWMBlurGlass")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DWMBG_AeroCalculator")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyProduct("Aero intensity calculator for DWMBlurGlass")]
+[assembly: AssemblyCopyright("Copyright © CatmanFan / Mr. Lechkar 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
